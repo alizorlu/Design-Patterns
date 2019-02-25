cb2a86c277870a684118c1711b5a5aa99172fd4a
alizorlu <azorlua@gmail.com>
2019-02-25 15:09:55 +0300

    BUILDER PATTERN

---
+++ Design_Patterns/BUILDER.PATTERN/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BUILDER.PATTERN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StudentDebtDirector director = new StudentDebtDirector();
+
+            StudentDebtDirector.student = new StudentViewModel()
+            {
+                Id = 1,
+                IsMale = false,
+                Name = "Zeliha",
+                Surname = "Kocabaş",
+                Paid = 2000m
+            };
+            var builder = new FemaleStudentDebtBuilder();
+            director.GenerateStudentDebt(builder);
+            var resultmodel = builder.GetStudentData(StudentDebtDirector.student);
+
+        }
+    }
+    public class StudentViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool IsMale { get; set; }
+        public decimal Paid { get; set; }
+    }
+    public abstract class StudentBuilder
+    {
+        public abstract StudentViewModel GetStudentData(StudentViewModel model);
+        public abstract void DeptStudent(StudentViewModel model);
+    }
+    public class FemaleStudentDebtBuilder : StudentBuilder
+    {
+        public override void DeptStudent(StudentViewModel model)
+        {
+            model.Paid = model.Paid * 0.05m;
+        }
+
+        public override StudentViewModel GetStudentData(StudentViewModel model)
+        {
+            return model;
+        }
+    }
+    public class MaleStudentDebtBuilder : StudentBuilder
+    {
+        public override void DeptStudent(StudentViewModel model)
+        {
+            model.Paid = model.Paid * 0.04m;
+        }
+
+        public override StudentViewModel GetStudentData(StudentViewModel model)
+        {
+            return model;
+        }
+    }
+    public class StudentDebtDirector
+    {
+        public static StudentViewModel student;
+        private object _lock = new object();
+        public StudentViewModel CreateStudent()
+        {
+            lock (_lock)
+            {
+                if (student == null) student = new StudentViewModel();
+            }
+            return student;
+        }
+        public void GenerateStudentDebt(StudentBuilder builder)
+        {
+            builder.GetStudentData(student);
+            builder.DeptStudent(student);
+        }
+    }
+}
---
+++ Design_Patterns/BUILDER.PATTERN/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("BUILDER.PATTERN")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BUILDER.PATTERN")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("cc74b2f7-0a77-4514-933b-add5142fed89")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
