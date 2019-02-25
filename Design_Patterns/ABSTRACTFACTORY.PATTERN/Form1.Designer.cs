7855c3215e71357bdd7c4204c4745b9046b54943
alizorlu <azorlua@gmail.com>
2019-02-25 11:19:19 +0300

    ABSTRACT FACTORY

---
+++ Design_Patterns/ABSTRACTFACTORY.PATTERN/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ABSTRACTFACTORY.PATTERN
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adminBtn = new System.Windows.Forms.Button();
+            this.editorBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // adminBtn
+            // 
+            this.adminBtn.Location = new System.Drawing.Point(12, 17);
+            this.adminBtn.Name = "adminBtn";
+            this.adminBtn.Size = new System.Drawing.Size(297, 74);
+            this.adminBtn.TabIndex = 0;
+            this.adminBtn.Text = "Admin Operation";
+            this.adminBtn.UseVisualStyleBackColor = true;
+            this.adminBtn.Click += new System.EventHandler(this.adminBtn_Click);
+            // 
+            // editorBtn
+            // 
+            this.editorBtn.Location = new System.Drawing.Point(12, 97);
+            this.editorBtn.Name = "editorBtn";
+            this.editorBtn.Size = new System.Drawing.Size(297, 74);
+            this.editorBtn.TabIndex = 1;
+            this.editorBtn.Text = "Editor Operation";
+            this.editorBtn.UseVisualStyleBackColor = true;
+            this.editorBtn.Click += new System.EventHandler(this.editorBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 189);
+            this.Controls.Add(this.editorBtn);
+            this.Controls.Add(this.adminBtn);
+            this.Name = "Form1";
+            this.Text = "ABSTRACT FACTORY";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button adminBtn;
+        private System.Windows.Forms.Button editorBtn;
+    }
+}
+
---
+++ Design_Patterns/ABSTRACTFACTORY.PATTERN/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ABSTRACTFACTORY.PATTERN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void adminBtn_Click(object sender, EventArgs e)
+        {
+
+            OperationModel.administrator = new AdministratorUser();
+            OperationModel.administrator.email = "azorlua@gmail.com";
+            OperationModel.administrator.username = "azorlua";
+            OperationModel.teacher = new TeacherRegister();
+            OperationModel.teacher.Name = "Engin";
+            OperationModel.teacher.Surname = "Avcı";
+            OperationModel.teacher.Lesson = "Image Processing";
+            OperationModel model = new OperationModel(new SystemAuth());
+
+            model.GetAll();
+        }
+
+        private void editorBtn_Click(object sender, EventArgs e)
+        {
+            OperationModel.student = new StudentRegister();
+            OperationModel.student.Education = "Fırat Üniversitesi";
+            OperationModel.student.Name = "Ali";
+            OperationModel.student.Surname = "Zorlu";
+            OperationModel.customer = new Customer();
+            OperationModel.customer.email = "musteri@emagazam.com";
+            OperationModel.customer.job = "Serbest Meslek";
+            OperationModel model = new OperationModel(new Auth());
+
+            model.GetAll();
+        }
+    }
+    public abstract class Register
+    {
+        public abstract void Regist(object model);
+    }
+    public class StudentRegister : Register
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Education { get; set; }
+        public override void Regist(object model)
+        {
+            StudentRegister student
+                = (StudentRegister)model;
+            MessageBox.Show($"{student.Name}{student.Surname} öğrencinin kaydı başarılı {DateTime.Now.ToString("HH:mm")}");
+        }
+    }
+    public class TeacherRegister : Register
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Lesson { get; set; }
+        public override void Regist(object model)
+        {
+            TeacherRegister teacher
+                = (TeacherRegister)model;
+            MessageBox.Show($"{teacher.Name}{teacher.Surname} " +
+                $"öğretmenin kaydı başarılı[{teacher.Lesson}] {DateTime.Now.ToString("HH:mm")}");
+        }
+    }
+    public abstract class SystemUser
+    {
+        public abstract void CreateUser(object model);
+    }
+    public class AdministratorUser : SystemUser
+    {
+        public string username { get; set; }
+        public string email { get; set; }
+        public override void CreateUser(object model)
+        {
+            AdministratorUser administrator
+                = (AdministratorUser)model;
+            MessageBox.Show($"Yeni admin oluşturuldu.\n{administrator.email}");
+        }
+    }
+    public class Customer : SystemUser
+    {
+        public string email { get; set; }
+        public string job { get; set; }
+        public override void CreateUser(object model)
+        {
+            Customer customer = (Customer)model;
+            MessageBox.Show($"Yeni müşteri[{customer.job}] oluşturuldu.\n{customer.email}");
+        }
+    }
+    public abstract class SystemAuthFactory
+    {
+        public abstract Register CreateRegister();
+        public abstract SystemUser CreateSystemUser();
+    }
+    public class SystemAuth : SystemAuthFactory
+    {
+        public override Register CreateRegister()
+        {
+            return new TeacherRegister();
+        }
+
+        public override SystemUser CreateSystemUser()
+        {
+            return new AdministratorUser();
+        }
+    }
+    public class Auth : SystemAuthFactory
+    {
+        public override Register CreateRegister()
+        {
+            return new StudentRegister();
+        }
+
+        public override SystemUser CreateSystemUser()
+        {
+            return new Customer();
+        }
+    }
+    public class OperationModel
+    {
+        private SystemAuthFactory systemAuthFactory;
+        private SystemUser systemUser;
+        private Register register;
+        private object _lock = new object();
+        public static AdministratorUser administrator;
+        public static TeacherRegister teacher;
+        public static Customer customer;
+        public static StudentRegister student;
+        public OperationModel(SystemAuthFactory systemAuthFactory)
+        {
+            this.systemAuthFactory = systemAuthFactory;
+            systemUser = this.systemAuthFactory.CreateSystemUser();
+            
+            //systemUser.CreateUser(administrator); //EDITOR OPERATION BEFORE COMMENT
+            systemUser.CreateUser(customer);
+            register = this.systemAuthFactory.CreateRegister();
+            //register.Regist(teacher); //EDITOR OPERATION BEFORE COMMENT
+            register.Regist(student);
+        }
+        public void GetAll()
+        {
+            MessageBox.Show("Tüm operasyonlar gerçekleşti");
+        }
+    }
+}
