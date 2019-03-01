e187bf48ad00c9d6d523d48f0fdf378b8992e5da
alizorlu <azorlua@gmail.com>
2019-03-01 11:35:53 +0300

    DECORATOR,PROXY,BRIDGE

---
+++ Design_Patterns/BRIDGE.PATTERN/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRIDGE.PATTERN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentManger paymentManger
+                = new PaymentManger();
+            paymentManger.NewPayment = new IyzicoService();
+            paymentManger.payment = new Payment()
+            {
+                From = "Ali Zorlu",
+                To = "HepsiOrada INC",
+                Money = 475.99m
+            };
+            paymentManger.SaleProduct();
+            Console.ReadKey();
+        }
+    }
+    abstract class PaymentServiceBase
+    {
+        public void Logging()
+        {
+            Console.WriteLine("Payment received");
+        }
+        public abstract void Receiving(Payment payment);
+    }
+    public class Payment
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal Money { get; set; }
+    }
+    class PaypalService : PaymentServiceBase
+    {
+        public override void Receiving(Payment payment)
+        {
+            Console.WriteLine($"{payment.From}=>{payment.To} received =TRY{payment.Money} [ via Paypal Service ]");
+        }
+    }
+    class IyzicoService : PaymentServiceBase
+    {
+        public override void Receiving(Payment payment)
+        {
+            Console.WriteLine($"{payment.From}=>{payment.To} received =TRY{payment.Money} [ via Iyzico Service ]");
+        }
+    }
+
+    class PaymentManger
+    {
+        public PaymentServiceBase NewPayment { get; set; }
+        public Payment payment{ get; set; }
+        public void SaleProduct()
+        {
+            NewPayment.Receiving(payment);
+            NewPayment.Logging();
+            Console.WriteLine("Product saled.Thank you");
+        }
+    }
+}
---
+++ Design_Patterns/DECORATOR.PATTERN/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DECORATOR.PATTERN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductBase product = new MarketProducts()
+            {
+                Name = "Ülker Çikolatalı Gofret",
+                Unit = "Adet/1",
+                Price = 1.35m
+            };
+            Console.WriteLine($"{product.Name}={product.Price}TL");
+            SpecialOffer special = new SpecialOffer(product);
+            special.DiscountPercentage = 10;
+            Console.WriteLine($"{special.Name}={special.Price}TL");
+            Console.ReadKey();
+        }
+    }
+    abstract class ProductBase
+    {
+        public abstract string Name { get; set; }
+        public abstract string Unit { get; set; }
+        public abstract decimal Price { get; set; }
+    }
+    class MarketProducts : ProductBase
+    {
+        public override string Name { get;set; }
+        public override string Unit { get; set; }
+        public override decimal Price { get; set; }
+    }
+    class ButcherProducts : ProductBase
+    {
+        public override string Name { get; set; }
+        public override string Unit { get; set; }
+        public override decimal Price { get; set; }
+    }
+    abstract class ProductDecoratorBase : ProductBase
+    {
+        private ProductBase _product;
+        protected ProductDecoratorBase(ProductBase product)
+        {
+            _product = product;
+        }
+    }
+    class SpecialOffer : ProductDecoratorBase
+    {
+        private readonly ProductBase product;
+        public int DiscountPercentage { get; set; } = 0;
+        public SpecialOffer(ProductBase _product):base(_product)
+        {
+            product = _product;
+        }
+        public override string Name { get; set; }
+        public override string Unit { get; set; }
+        public override decimal Price { get { return product.Price - (product.Price * DiscountPercentage / 100); }
+            set {  } }
+    }
+}
