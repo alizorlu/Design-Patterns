f0b37eeeb493800a3ad329f3506ee7aa55977768
alizorlu <azorlua@gmail.com>
2019-03-01 14:13:22 +0300

    STRATEGY PATTERN

---
+++ Design_Patterns/STRATEGY.PATTERN/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STRATEGY.PATTERN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditManager manager = new CreditManager();
+            manager.customer = new Customer()
+            {
+                Name = "Ali",
+                Surname = "Zorlu",
+                TargetCredit = 35500.00m
+            };
+            Console.WriteLine($"Talep Edilen Kredi:{manager.customer.TargetCredit}");
+            Console.WriteLine("======");
+            manager.qonyabank = new After2010CustomerCreditCalculator();
+            manager.GenerateCredit();
+
+            Console.WriteLine("======");
+            manager.qonyabank = new Before2010CustomerCreditCalculator();
+            manager.GenerateCredit();
+
+            Console.ReadKey();
+        }
+    }
+    abstract class QonyaBankCreditBase
+    {
+        public abstract void CalcCredit(Customer customer);
+    }
+    class After2010CustomerCreditCalculator : QonyaBankCreditBase
+    {
+        public override void CalcCredit(Customer customer)
+        {
+            Console.WriteLine($"Müşteri:{customer.Name}-{customer.Surname}\nHesaplanan Kredi:TRY{customer.TargetCredit+customer.TargetCredit*0.10m}");
+        }
+    }
+    class Before2010CustomerCreditCalculator : QonyaBankCreditBase
+    {
+        public override void CalcCredit(Customer customer)
+        {
+            Console.WriteLine($"Müşteri:{customer.Name}-{customer.Surname}\nHesaplanan Kredi:TRY{customer.TargetCredit + customer.TargetCredit * 0.08m}");
+        }
+    }
+    class CreditManager
+    {
+        public QonyaBankCreditBase qonyabank { get; set; }
+        public Customer customer { get; set; }
+
+        public void GenerateCredit()
+        {
+            Console.WriteLine("Kredi hesaplamanız şu şekildedir:\n");
+            qonyabank.CalcCredit(customer);
+        }
+    }
+    class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal TargetCredit { get; set; }
+    }
+}
---
+++ Design_Patterns/STRATEGY.PATTERN/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("STRATEGY.PATTERN")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("STRATEGY.PATTERN")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("a5562a36-af43-4463-b9c9-2ed376f154e2")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
