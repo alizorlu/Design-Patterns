32b039d103e29f750c822c78d2337a98f45c0b23
alizorlu <azorlua@gmail.com>
2019-03-01 14:59:31 +0300

    OBSERVER PATTERN

---
+++ Design_Patterns/OBSERVER.PATTERN/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBSERVER.PATTERN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewsManager manager = new NewsManager();
+            manager.news = new News()
+            {
+                Title = "Ortamın Adını Koyduk!",
+                Content = "Fikret Orman,bjkstore marketiyle piyasada takıma ait ürünlerin satışını yapacaklarını söyledi",
+                AddingTime = DateTime.Now
+            };
+            Observer visitor = new VisitorObserver();
+            manager.Attach(visitor);
+            visitor.ChangeNews();
+            manager.Attach(new SubscriberObserver());
+            manager.AddNews();
+            Console.ReadLine();
+        }
+    }
+    class NewsManager
+    {
+        List<Observer> _observer = new List<Observer>();
+        public News news { get; set; }
+        public void AddNews()
+        {
+            Console.WriteLine($"Son dakika !\n{news.Title.ToUpper()}\n{news.Content}\nTarih:{news.AddingTime.ToShortDateString()}");
+            
+        }
+        public void Attach(Observer observer)
+        {
+            _observer.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            _observer.Remove(observer);
+        }
+        private void NotifyAll()
+        {
+            foreach (var item in _observer)
+            {
+                AddNews();
+            }
+        }
+    }
+    abstract class Observer
+    {
+       
+        public abstract void ChangeNews();
+       
+    }
+    class VisitorObserver : Observer
+    {
+        public override void ChangeNews()
+        {
+            Console.WriteLine($"[Ziyaretçi]:Biraz önce şöyle bir haber yayınlandı.");
+        }
+    }
+    class SubscriberObserver : Observer
+    {
+        public override void ChangeNews()
+        {
+            Console.WriteLine($"[Abone]:Biraz önce şöyle bir haber yayınlandı.");
+        }
+    }
+    public class News
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime AddingTime { get; set; }
+    }
+}
---
+++ Design_Patterns/OBSERVER.PATTERN/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("OBSERVER.PATTERN")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OBSERVER.PATTERN")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("6ca428e0-3d16-40e8-b7c4-a74bb4dba9af")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
